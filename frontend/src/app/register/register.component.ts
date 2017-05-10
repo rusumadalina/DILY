8a317ff8ddb82c9dc3617b6bbb68a355f887c9cb
ANDRA-PC\Andra <andra01spiridon@yahoo.com>
2017-05-10 23:45:48 +0300

    [FrontEnd] Added register Component

---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {RegisterService} from './register.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  providers: [RegisterService]
+})
+export class RegisterComponent implements OnInit {
+  registerValue : string;
+  constructor(private _httpService: RegisterService) { }
+
+  ngOnInit() {
+  }
+
+  registerFunction(register: any) {
+    this._httpService.registerJSON(register)
+      .subscribe(
+        data => {this.registerValue = JSON.stringify(data); },
+        error => {alert(error); },
+        () => console.log('Finished')
+      );
+  }
+}
