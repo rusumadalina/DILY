b7d2ce6a2674490225cb62f485508c58c7ebca65
rusu.madalina <rusu.madalina21@gmail.com>
2017-05-11 10:08:29 +0300

    [Register] getting data

--- src/main/java/com/dily/services/AuthenticationService.java
+++ src/main/java/com/dily/services/AuthenticationService.java
@@ -1,15 +1,33 @@
 package com.dily.services;
 
+import com.dily.Database;
 import com.dily.models.UserModel;
-import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by rusum on 09.05.2017.
  */
-@Service
-public interface AuthenticationService {
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Override
+    public UserModel findByUsernameAndPassword(String username, String password) throws SQLException {
 
-    public UserModel findByUsernameAndPassword (String username, String password) throws SQLException;
+        Connection con = Database.getConnection();
+
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from user_table where username ='" + username + "' and password = '" + password + "'");
+
+        UserModel userModel = new UserModel();
+        while (rs.next()) {
+            userModel.setUsername(rs.getString(3));
+            userModel.setPassword(rs.getString(4));
+        }
+
+        rs.close();
+
+        return userModel;
+    }
 }
---
+++ src/main/java/com/dily/services/IAuthenticationService.java
@@ -0,0 +1,15 @@
+package com.dily.services;
+
+import com.dily.models.UserModel;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+/**
+ * Created by rusum on 09.05.2017.
+ */
+@Service
+public interface AuthenticationService {
+
+    public UserModel findByUsernameAndPassword (String username, String password) throws SQLException;
+}
