5949843edef4e4bb8d64fb86a7a6c45d100ab5dd
DESKTOP-ULND8S0\rusum <rusu.madalina21@gmail.com>
2017-05-25 22:08:11 +0300

    [Database] Populare memory

---
+++ database/populare_memory.sql
@@ -0,0 +1,48 @@
+insert into memory 
+(MEMORYID, TITLE, DESCRIPTION,MEMORYLOCATION,DATEM, PRIVACY, MAINPICTURE)
+values
+(1, 'Prima data la paris!', 'A fost foarte frumos, minunat, mirobolant! Orasul luminilor!', 'Paris, Franta', TO_DATE('18/06/2016', 'dd/mm/yyyy'),'public','assets/images/memory/memory1.jpg');
+
+insert into memory
+(MEMORYID, TITLE, DESCRIPTION,MEMORYLOCATION,DATEM, PRIVACY, MAINPICTURE)
+values
+(2, 'Budapest!!!', 'A inceput vacanta!!! Yeeeey! Revenim cu poze!!','Budapest', TO_DATE('10/06/2016', 'dd/mm/yyyy'),'public','assets/images/memory/memory2.jpg');
+
+insert into memory
+(MEMORYID, TITLE, DESCRIPTION,MEMORYLOCATION,DATEM, PRIVACY, MAINPICTURE)
+values
+(3, 'Zapadaaaa!!!', 'Trecem prin maramures si a nins! Poate ne oprim sa facem un om de zapada!', 'Maramures', TO_DATE('06/02/2015', 'dd/mm/yyyy'),'public','assets/images/memory/memory3.jpg');
+
+insert into memory (MEMORYID, TITLE, DESCRIPTION,MEMORYLOCATION,DATEM, PRIVACY, MAINPICTURE)
+values
+(4, 'Cu totii pe barca', 'A fost frig dar frumos! o experienta minunata!', 'Paris, Franta', TO_DATE('10/06/2010', 'dd/mm/yyyy'),'public','assets/images/memory/memory4.jpg');
+
+insert into memory
+(MEMORYID, TITLE, DESCRIPTION,MEMORYLOCATION,DATEM, PRIVACY, MAINPICTURE)
+values
+(5, 'Parlamentul European', 'Am votat legi astazi! iuhuuu', 'Strasbourg', TO_DATE('17/03/2013', 'dd/mm/yyyy'),'public','assets/images/memory/memory5.jpg');
+
+insert into memory
+(MEMORYID, TITLE, DESCRIPTION,MEMORYLOCATION,DATEM, PRIVACY, MAINPICTURE)
+values
+(6, 'Praga', 'Astazi am ajuns si in Praga. Speram sa revenim!', 'Paga', TO_DATE('12/05/2016', 'dd/mm/yyyy'),'public','assets/images/memory/memory6.jpg');
+
+insert into memory 
+(MEMORYID, TITLE, DESCRIPTION,MEMORYLOCATION,DATEM, PRIVACY, MAINPICTURE)
+values
+(7, 'Lebede', 'Inplimbarea noastra de astazi am intalnit niste lebede tare simpatice!!', 'Starsbourg', TO_DATE('15/06/2016', 'dd/mm/yyyy'),'public','assets/images/memory/memory7.jpg');
+
+insert into memory 
+(MEMORYID, TITLE, DESCRIPTION,MEMORYLOCATION,DATEM, PRIVACY, MAINPICTURE)
+values
+(8, 'Briose', 'De 1 martie le-am facut baietilor briose! Sper sa le placa', 'Iasi', TO_DATE('01/03/2016', 'dd/mm/yyyy'),'public','assets/images/memory/memory8.jpg');
+
+insert into memory 
+(MEMORYID, TITLE, DESCRIPTION,MEMORYLOCATION,DATEM, PRIVACY, MAINPICTURE)
+values
+(9, 'Majorat', 'Astazi prietena noastra a implinit 18 ani! A fost o petrecere minunata! La multi ani!!!!!', 'Iasi', TO_DATE('18/06/2017', 'dd/mm/yyyy'),'public','assets/images/memory/memory9.jpg');
+
+insert into memory
+(MEMORYID, TITLE, DESCRIPTION,MEMORYLOCATION,DATEM, PRIVACY, MAINPICTURE)
+values
+(10, 'Absolvire <3', 'Am absolit liceul! A fost o zi plina de emotii', 'Iasi', TO_DATE('14/02/2016', 'dd/mm/yyyy'),'public','assets/images/memory/memory10.jpg');
