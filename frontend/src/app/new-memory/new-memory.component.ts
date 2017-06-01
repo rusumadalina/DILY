71f25ac254f2f6f66e8b35ab0ba1697d4160e356
DESKTOP-ULND8S0\rusum <rusu.madalina21@gmail.com>
2017-06-01 22:36:27 +0300

    [FronEnd BackEnd] add memory

--- frontend/src/app/new-memory/new-memory.component.ts
+++ frontend/src/app/new-memory/new-memory.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {NewMemoryService} from "./new-memory.service";
 import {Friend} from "../model/friend.model";
+import {User} from "../model/user.model";
 
 @Component({
   selector: 'app-new-memory',
   templateUrl: './new-memory.component.html',
-  styleUrls: ['../settings/settings.component.scss'],
+  styleUrls: ['./new-memory.component.scss'],
   providers: [NewMemoryService ]
 })
 export class NewMemoryComponent implements OnInit {
   friends = [];
-  constructor(private newMemoryService: NewMemoryService) { }
+  toggle : boolean;
+  text: string;
+  curent_user: User;
+  tagged =[];
+  memId : string;
+  tags=[];
+  files=[];
+  files2=[];
+  curentStep: number;
+  constructor(private newMemoryService: NewMemoryService, private eRef: ElementRef) {
+    this.toggle=false;
+    this.curent_user = JSON.parse(localStorage.getItem('user'));
+    this.files2=JSON.parse(localStorage.getItem('pictures'));
+    this.memId=localStorage.getItem('memoryId');
+    this.curentStep=1;
+    if(localStorage.getItem('step')===null){
+      localStorage.setItem('step','1');
+    }else{
+      this.curentStep = parseInt(localStorage.getItem('step'));
+    }
+  }
 
   ngOnInit() {
     this.newMemoryService.getAllFriends().subscribe(
@@ -26,4 +47,117 @@ export class NewMemoryComponent implements OnInit {
     }
   }
 
+  setToggle(event) {
+    let clickType: boolean;
+    if (this.eRef.nativeElement.contains(event.target)) {
+      clickType = true;
+    } else {
+      clickType = false;
+    }
+
+    if (clickType === false) {
+      this.toggle = false;
+    }
+    if (clickType === true && this.toggle === false){
+      this.toggle = true;
+    }
+  }
+
+  setSearch(name: string) {
+    this.text = name;
+    this.toggle=false;
+    localStorage.setItem('privacyMem', this.text);
+  }
+
+  onChange(event) {
+    const files = event.srcElement.files;
+    this.newMemoryService.makeFileRequest('http://localhost:8072/api/newMemory/upload', [], files).subscribe(() => {
+      console.log('sent');
+    });
+  }
+  submitMemory(addMemory: any) {
+    this.newMemoryService.postJSON(addMemory).subscribe(
+      data => {
+        this.memId=data.toString();
+        let aux=data.toString();
+        localStorage.setItem('memoryId',aux);
+        console.log(data);},
+      error => {console.log('nu ai postat'); }
+    );
+    //this.curentStep=parseInt(localStorage.getItem('step'));
+    localStorage.setItem('step','2');
+    this.curentStep=parseInt(localStorage.getItem('step'));
+    //this._router.navigate(['dashboard']);
+
+  }
+  selectFriends(id: number){
+    let aux=false;
+   // if(this.tagged.filter(tagg => tagg.name === this.tagged.name))
+
+    for(let index of this.tagged){
+      if(index === id){
+        aux=true;
+        this.tagged.splice(this.tagged.indexOf(index),1);
+      }
+    }
+    if(aux===false){
+      this.tagged.push(id);
+    }
+    console.log(this.tagged);
+    }
+  submitTagged(){
+    this.curentStep=parseInt(localStorage.getItem('step'));
+      let array=[];
+      for(let index of this.tagged){
+        array.push({"friendId": index})
+      }
+    this.newMemoryService.postTagged(array, this.memId).subscribe(
+      (data) => {console.log(data); },
+      (err) => alert(err));
+
+  }
+
+  submitTags(addTags: any){
+    this.curentStep=parseInt(localStorage.getItem('step'));
+    let array1= addTags.tags.split(' ');
+
+    for (let item of array1){
+      this.tags.push({"tagName":item.split('#')[1]});
+    }
+    console.log(this.tags);
+    this.newMemoryService.postTags(this.tags, this.memId).subscribe(
+
+      (data) => {console.log(data); },
+      (err) => alert(err));
+    localStorage.setItem('step','4');
+    this.curentStep=parseInt(localStorage.getItem('step'));
+  }
+
+  onChangeMedia(event) {
+    const files = event.srcElement.files;
+
+    if(JSON.parse(localStorage.getItem('pictures'))!=null){
+      const aux=JSON.parse(localStorage.getItem('pictures'));
+      aux.push('assets/images/memory/'+files[0].name);
+      localStorage.setItem('pictures',JSON.stringify(aux));
+    }else{
+      const aux=[];
+      aux.push('assets/images/memory/'+files[0].name);
+      localStorage.setItem('pictures',JSON.stringify(aux));
+    }
+
+    this.newMemoryService.makeFileRequestPictures('http://localhost:8072/api/newMemory/uploadPictures', [], files).subscribe(() => {
+      console.log('sent');
+    });
+    localStorage.setItem('step','2');
+  }
+  submitMedia(){
+    let aux=JSON.parse(localStorage.getItem('pictures'));
+    this.newMemoryService.postMedia(aux, this.memId).subscribe(
+
+      (data) => {console.log(data); localStorage.removeItem('pictures'); },
+      (err) => alert(err));
+    localStorage.setItem('step','3');
+    this.curentStep=parseInt(localStorage.getItem('step'));
+  }
 }
--- frontend/src/app/new-memory/new-memory.service.ts
+++ frontend/src/app/new-memory/new-memory.service.ts
@@ -1,12 +1,18 @@
 import {Injectable} from "@angular/core";
 import {User} from "../model/user.model";
 import {Http, Headers} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 @Injectable()
 export class NewMemoryService {
   curent_user: User;
-
+  mainMem: string;
+  privacy:string;
+  medias=[];
   constructor(private http: Http) {
     this.curent_user = JSON.parse(localStorage.getItem('user'));
+    this.mainMem=localStorage.getItem('memoryMainPhoto');
+    this.privacy=localStorage.getItem('privacyMem');
+
   }
 
   getAllFriends() {
@@ -15,6 +21,105 @@ export class NewMemoryService {
 
     return this.http.get(
       'http://localhost:8072/api/newMemory/friends/' + this.curent_user.user_id,
+      {headers: headers})
+      .map(res => res.json());
+  }
+
+
+  public makeFileRequest (url: string, params: string[], files: File[]){
+    return Observable.create(observer => {
+      const formData: FormData = new FormData(),
+        xhr: XMLHttpRequest = new XMLHttpRequest();
+
+      for (let i = 0; i < files.length; i++) {
+        formData.append('uploads[]', files[i], files[i].name);
+      }
+      localStorage.setItem('memoryMainPhoto', 'assets/images/mainPictures/' + files[0].name);
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            observer.next(JSON.parse(xhr.response));
+            observer.complete();
+          } else {
+            observer.error(xhr.response);
+          }
+        }
+      };
+      xhr.open('POST', url, true);
+      xhr.send(formData);
+    });
+  }
+
+  postJSON(addMemory) {
+    const json = JSON.stringify(
+      {
+        title: addMemory.title,
+        description: addMemory.description,
+        memoryLocation: addMemory.location,
+        date: addMemory.date,
+        privacy: this.privacy,
+        mainPicture: this.mainMem
+      });
+
+    const header =  new Headers();
+    header.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:8072/api/addMemory/'+ this.curent_user.user_id,  json, { headers: header }).map(res => res.json());
+  }
+
+  postTagged(array: any[], id: string){
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post(
+      'http://localhost:8072/api/addMemory/tagged/' + id, array,
+      {headers: headers})
+      .map(res => res.json());
+  }
+
+  postTags(array: any[], id: string){
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post(
+      'http://localhost:8072/api/addMemory/tags/' + id, array,
+      {headers: headers})
+      .map(res => res.json());
+  }
+
+
+  public makeFileRequestPictures (url: string, params: string[], files: File[]){
+    return Observable.create(observer => {
+      const formData: FormData = new FormData(),
+        xhr: XMLHttpRequest = new XMLHttpRequest();
+
+      for (let i = 0; i < files.length; i++) {
+        formData.append('uploads[]', files[i], files[i].name);
+      }
+     // localStorage.setItem('memoryMainPhoto', 'assets/images/memory/' + files[0].name);
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            observer.next(JSON.parse(xhr.response));
+            observer.complete();
+          } else {
+            observer.error(xhr.response);
+          }
+        }
+      };
+      xhr.open('POST', url, true);
+      xhr.send(formData);
+    });
+
+  }
+  postMedia(array: string[], id:string){
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    for(let index of array){
+      this.medias.push({"picture":index})
+    }
+    return this.http.post(
+      'http://localhost:8072/api/addMemory/media/' + id, this.medias,
       {headers: headers})
       .map(res => res.json());
   }
